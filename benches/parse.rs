edf3e0e2e8426a5f4dd555f19df6dc8c6a70ea29
Josh Lindsey <joshua.s.lindsey@gmail.com>
2020-09-23 09:28:37 -0400

    increase the bench complexity

--- benches/parse.rs
+++ benches/parse.rs
@@ -1,24 +1,38 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use hcl2::ast::parse_str;
 
-const TEST_BODY: &str = "test block \"label\" {
+const TEST_BODY: &str = r#"
+test block "label" {
+    a = 1.4e9
+    b = -73.19e-12
+
     internal {
-        a = \"test string here\"
+        a = "test string here"
         b = 5
         c = 7.3
         d = -3
         e = -4.873
-        f = false
-        g = true
-        h = null
-        i = [1, 2, 3]
-        j = [false, null, \"string\"]
         k = <<EOF
         heredoc string
         is here, doc
         EOF
     }
-}\n";
+
+    block two {
+        a = false
+        b = true
+        c = null
+        d = [1, 2, 3]
+        e = [false, null, "string"]
+    }
+}
+
+another test block {
+    a = "another test string"
+    b = 1.7e10
+    c = false
+}
+"#;
 
 pub fn benchmark(c: &mut Criterion) {
     c.bench_function("parse hcl2", |b| b.iter(|| black_box(parse_str(TEST_BODY))));
